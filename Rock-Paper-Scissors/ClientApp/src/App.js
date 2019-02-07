06621e640c04834bf6db95959931b559a83dfae8
TSM Techø <trottimusbg@gmail.com>
2019-02-07 17:12:08 +0200

    [App] Add header main and footer and some styles.

--- Rock-Paper-Scissors/ClientApp/src/App.js
+++ Rock-Paper-Scissors/ClientApp/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import './App.scss';
+import logo from './assets/images/logo.svg'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <header>
-            <nav>
-                <div className="logo">
-                    
-                </div>
-            </nav>
+        <header className="intro">
+            <div className="logo">
+                <img src={logo} alt="" />
+            </div>
+            <form className="player-form">
+                <input type="text" className="player-username" placeholder="Enter your username"/>
+                <button type="button" className="btn btn--primary btn--inside uppercase">Confirm</button>
+            </form>
         </header>
         <main></main>
         <footer></footer>
