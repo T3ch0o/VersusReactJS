72a3511edff28a305b4d824ed8ea33748a1aceef
TSM Techø <trottimusbg@gmail.com>
2019-02-08 11:54:33 +0200

    [BaseApiController] Add controller from which all other controllers will depend on.

---
+++ Rock-Paper-Scissors/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+﻿namespace Rock_Paper_Scissors.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
