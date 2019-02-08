87273cc46a09746d620d3cfa6fbd16b225863b7d
TSM Techø <trottimusbg@gmail.com>
2019-02-08 21:58:43 +0200

    [playerReducer] Add loggeIn state.

--- Rock-Paper-Scissors/ClientApp/src/store/reducers/playerReducer.js
+++ Rock-Paper-Scissors/ClientApp/src/store/reducers/playerReducer.js
@@ -1,9 +1,11 @@
-import { REGISTER_PLAYER } from '../actions/actionTypes';
+import { LOGGED_IN_PLAYER, REGISTER_PLAYER } from '../actions/actionTypes';
 
-export default function playerReducer(state = {}, action) {
+export default function playerReducer(state = { loggedIn: false }, action) {
     switch (action.type) {
         case REGISTER_PLAYER:
             return Object.assign({}, state);
+        case LOGGED_IN_PLAYER:
+            return Object.assign({}, state, { loggedIn: true });
         default:
             return state;
     }
