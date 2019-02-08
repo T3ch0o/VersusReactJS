f83a7f1a0da3c3be20ef9922cbbf2f1183738a91
TSM Techø <trottimusbg@gmail.com>
2019-02-08 11:49:24 +0200

    [App] Add connection to the store and submit player username.

--- Rock-Paper-Scissors/ClientApp/src/App.js
+++ Rock-Paper-Scissors/ClientApp/src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
 import './App.scss';
+import dataCollector from './utils/dataCollector';
 import logo from './assets/images/logo.svg'
+import { registerPlayerAction } from './store/actions/playerActions';
+import connect from 'react-redux/es/connect/connect';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <header className="intro">
-            <div className="logo">
-                <img src={logo} alt="" />
+    constructor() {
+        super();
+
+        this.state = {
+            username: ''
+        };
+
+        this.onSubmitHandler = this.onSubmitHandler.bind(this);
+    }
+
+    onSubmitHandler(event) {
+        event.preventDefault();
+
+        this.props.register({username: this.state.username});
+    }
+
+    render() {
+        return (
+            <div>
+                <header className="intro">
+                    <div className="logo">
+                        <img src={logo} alt="" />
+                    </div>
+                    <form className="player-form" onSubmit={this.onSubmitHandler}>
+                        <input name="username" type="text" className="player-username" placeholder="Enter your username" onChange={dataCollector.bind(this)}/>
+                        <button type="submit" className="btn btn--primary btn--inside uppercase">Confirm</button>
+                    </form>
+                </header>
+                <main>
+
+                </main>
+                <footer>
+
+                </footer>
             </div>
-            <form className="player-form">
-                <input type="text" className="player-username" placeholder="Enter your username"/>
-                <button type="button" className="btn btn--primary btn--inside uppercase">Confirm</button>
-            </form>
-        </header>
-        <main></main>
-        <footer></footer>
-      </div>
-    );
-  }
+        );
+    }
 }
 
-export default App;
+function mapStateToProps(state) {
+    return { };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        register: (payload) => dispatch(registerPlayerAction(payload))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
