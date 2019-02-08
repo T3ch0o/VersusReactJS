d306a93c069154d91ac21a66cec06c9b1889f521
TSM Techø <trottimusbg@gmail.com>
2019-02-08 11:55:21 +0200

    [Player] Add model.

---
+++ Rock-Paper-Scissors/Entities/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace Rock_Paper_Scissors.Entities
+{
+    public class Player
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public int Score { get; set; }
+    }
+}
