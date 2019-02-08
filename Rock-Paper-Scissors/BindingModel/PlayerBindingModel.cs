f36a58e80b93b9471ee48f9231919101216a98bf
TSM Techø <trottimusbg@gmail.com>
2019-02-08 21:55:13 +0200

    [BindingModels] Change folder name.

--- Rock-Paper-Scissors/BindingModel/PlayerBindingModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Rock_Paper_Scissors.BindingModel
-{
-    public class PlayerBindingModel
-    {
-        public string Username { get; set; }
-    }
-}
---
+++ Rock-Paper-Scissors/BindingModels/PlayerBindingModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Rock_Paper_Scissors.BindingModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class PlayerBindingModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(10, MinimumLength = 4, ErrorMessage = "Username must be between 4 and 10 characters.")]
+        public string Username { get; set; }
+    }
+}
