3b427e5cefedb8b54cab0eb6aac525ca5114a139
TSM Techø <trottimusbg@gmail.com>
2019-02-08 21:56:49 +0200

    [playerActions] Add loggedInPlayerAction to change loggedIn state.

--- Rock-Paper-Scissors/ClientApp/src/store/actions/playerActions.js
+++ Rock-Paper-Scissors/ClientApp/src/store/actions/playerActions.js
@@ -1,11 +1,22 @@
 import player from '../../api/PlayerAPI';
+import { errorAction } from './ajaxActions';
+import { LOGGED_IN_PLAYER } from './actionTypes';
 
 export function registerPlayerAction(payload) {
-    console.log(payload);
     return function(dispatch) {
         return player.register(payload)
             .then(response => {
                 localStorage.setItem('username', response.data.username);
+                dispatch(loggedInPlayerAction());
             })
+            .catch(error => {
+                dispatch(errorAction());
+            });
     }
+}
+
+export function loggedInPlayerAction() {
+    return {
+        type: LOGGED_IN_PLAYER
+    };
 }
