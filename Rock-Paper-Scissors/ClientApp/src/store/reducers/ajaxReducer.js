7302e4b6ec17dd3ff0a43d970b53deb1bbd1f08c
TSM Techø <trottimusbg@gmail.com>
2019-02-09 00:17:29 +0200

    [ajaxReducer] Add message to the state.

--- Rock-Paper-Scissors/ClientApp/src/store/reducers/ajaxReducer.js
+++ Rock-Paper-Scissors/ClientApp/src/store/reducers/ajaxReducer.js
@@ -1,13 +1,13 @@
 import { AJAX_BEGIN, AJAX_SUCCESS, AJAX_ERROR} from '../actions/actionTypes';
 
-export default function authReducer(state = { begin: false, error: false }, action) {
+export default function authReducer(state = { begin: false, error: false, message: '' }, action) {
     switch (action.type) {
         case AJAX_BEGIN:
             return Object.assign({}, state, { begin: true, error: false });
         case AJAX_SUCCESS:
-            return Object.assign({}, state, { begin: false, error: false });
+            return Object.assign({}, state, { begin: false, error: false, message: '' });
         case AJAX_ERROR:
-            return Object.assign({}, state, { begin: false, error: true });
+            return Object.assign({}, state, { begin: false, error: true, message: action.message });
         default:
             return state;
     }
