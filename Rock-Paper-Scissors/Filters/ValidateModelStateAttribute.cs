a24dc27e015d90a78ff148453553489f9c44e51a
TSM Techø <trottimusbg@gmail.com>
2019-02-09 00:22:10 +0200

    [ValidateModelStateAttribute] Add customization of the BadRequestObjectResult when ModelState is invalid.

---
+++ Rock-Paper-Scissors/Filters/ValidateModelStateAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace Rock_Paper_Scissors.Filters
+{
+    using System.Linq;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    public class ValidateModelStateAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(new { message = context.ModelState.Values.SelectMany(m => m.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList()[0]
+                });
+            }
+        }
+    }
+}
