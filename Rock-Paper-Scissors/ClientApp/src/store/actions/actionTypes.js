afdffc3b473abd960290a488427fe173e4f243ac
TSM Techø <trottimusbg@gmail.com>
2019-02-08 21:55:52 +0200

    [actionTypes] Add ajax and logged in action types.

--- Rock-Paper-Scissors/ClientApp/src/store/actions/actionTypes.js
+++ Rock-Paper-Scissors/ClientApp/src/store/actions/actionTypes.js
@@ -1,1 +1,8 @@
+// Ajax Action Types
+export const AJAX_BEGIN = 'AJAX_BEGIN';
+export const AJAX_ERROR = 'AJAX_ERROR';
+export const AJAX_SUCCESS = 'AJAX_SUCCESS';
+
+// Player Action Types
 export const REGISTER_PLAYER = 'REGISTER_PLAYER';
+export const LOGGED_IN_PLAYER = 'LOGGED_IN_PLAYER';
