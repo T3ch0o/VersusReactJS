c50e8a23afd40b1891de983db9738609df6cd428
TSM Techø <trottimusbg@gmail.com>
2019-02-08 11:55:00 +0200

    [PlayerController] Add Register action.

---
+++ Rock-Paper-Scissors/Controllers/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿namespace Rock_Paper_Scissors.Controllers
+{
+    using BindingModel;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Services.Interfaces;
+
+    public class PlayerController : BaseApiController
+    {
+        private readonly IPlayerService _playerService;
+
+        public PlayerController(IPlayerService playerService)
+        {
+            _playerService = playerService;
+        }
+
+        [HttpPost("register")]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Register([FromBody] PlayerBindingModel player)
+        {
+            _playerService.RegisterPlayer(player.Username);
+
+            return Ok(player);
+        }
+    }
+}
