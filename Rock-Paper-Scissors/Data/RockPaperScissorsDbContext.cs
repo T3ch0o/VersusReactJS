f98591a2de8b5dbf0a7227c95ca13226d70b1580
TSM Techø <trottimusbg@gmail.com>
2019-02-09 00:20:46 +0200

    [RockPaperScissorsDbContext] Add context and DbSet of Players.

---
+++ Rock-Paper-Scissors/Data/RockPaperScissorsDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Rock_Paper_Scissors.Data
+{
+    using Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class RockPaperScissorsDbContext : DbContext
+    {
+        public RockPaperScissorsDbContext(DbContextOptions<RockPaperScissorsDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+    }
+}
