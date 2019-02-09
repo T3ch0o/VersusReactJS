6d96507faf04bc1fedd14bed8c2ae1752cd0e1c0
TSM Techø <trottimusbg@gmail.com>
2019-02-09 11:26:03 +0200

    [Rock_Paper_Scissors] Rename folder.

---
+++ Rock_Paper_Scissors/Middlewares/MiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Rock_Paper_Scissors.Middlewares
+{
+    using Microsoft.AspNetCore.Builder;
+
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseSeedDataMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestMiddleware>();
+        }
+    }
+}
---
+++ Rock_Paper_Scissors/Middlewares/RequestMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace Rock_Paper_Scissors.Middlewares
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+
+    public class RequestMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            string ip = httpContext.Connection.RemoteIpAddress.ToString();
+
+            await _next(httpContext);
+        }
+    }
+}
