e3fd353c15784433cf14736dbe7e5c447d706aa9
TSM Techø <trottimusbg@gmail.com>
2019-02-09 22:54:26 +0200

    [Player] Remove score property and add wins | losses | ties properties.

--- Rock_Paper_Scissors/Entities/Player.cs
+++ Rock_Paper_Scissors/Entities/Player.cs
@@ -6,7 +6,11 @@
 
         public string Username { get; set; }
 
-        public int Score { get; set; }
+        public int Wins { get; set; }
+
+        public int Losses { get; set; }
+
+        public int Ties { get; set; }
 
         public string IpAddress { get; set; }
     }
