aef8ce5e5b5360ca6559afec0e8040511702580d
TSM Techø <trottimusbg@gmail.com>
2019-02-09 16:38:48 +0200

    [App] Add FontAwesome Icons to the library.

--- Rock_Paper_Scissors/ClientApp/src/App.js
+++ Rock_Paper_Scissors/ClientApp/src/App.js
@@ -4,10 +4,16 @@ import './App.scss';
 // Components
 import Header from './components/common/Header';
 
+// FontAwesome Icons
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHandRock, faHandPaper, faHandPeace, faUserAltSlash } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faHandRock);
+library.add(faHandPaper);
+library.add(faHandPeace);
+library.add(faUserAltSlash);
+
 class App extends Component {
-    constructor() {
-        super();
-    }
 
     componentWillMount() {
         localStorage.clear();
