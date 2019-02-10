d09e948a8f1063b5f682b04952d64f367a011544
TSM Techø <trottimusbg@gmail.com>
2019-02-10 22:48:45 +0200

    [PlayerHandSignBindingModel] Add model to check username and his handSign.

---
+++ Rock_Paper_Scissors/BindingModels/PlayerHandSignBindingModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Rock_Paper_Scissors.BindingModels
+{
+    using System.ComponentModel.DataAnnotations;
+    using ValidationAttributes;
+
+    public class PlayerHandSignBindingModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(10, MinimumLength = 4, ErrorMessage = "Username must be between 4 and 10 characters.")]
+        public string Username { get; set; }
+
+        [Required]
+        [HandSign]
+        public string HandSign { get; set; }
+    }
+}
