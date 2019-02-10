7e1857d2bb2e612682c5c65301dc6ecb93f5b0b1
TSM Techø <trottimusbg@gmail.com>
2019-02-11 00:24:58 +0200

    [index] Add react reducer.

--- Rock_Paper_Scissors/ClientApp/src/store/reducers/index.js
+++ Rock_Paper_Scissors/ClientApp/src/store/reducers/index.js
@@ -1,7 +1,9 @@
 import ajaxReducer from './ajaxReducer';
 import playerReducer from './playerReducer';
+import reactReducer from './reactReducer';
 
 export default {
     ajax: ajaxReducer,
-    player: playerReducer
+    player: playerReducer,
+    react: reactReducer
 };
