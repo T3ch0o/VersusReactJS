c713a84807494db42b19675b53dd9b62be6a98ea
TSM Techø <trottimusbg@gmail.com>
2019-02-11 00:22:25 +0200

    [actionTypes] Add get react message.

--- Rock_Paper_Scissors/ClientApp/src/store/actions/actionTypes.js
+++ Rock_Paper_Scissors/ClientApp/src/store/actions/actionTypes.js
@@ -4,9 +4,8 @@ export const AJAX_ERROR = 'AJAX_ERROR';
 export const AJAX_SUCCESS = 'AJAX_SUCCESS';
 
 // Player Action Types
-export const REGISTER_PLAYER = 'REGISTER_PLAYER';
 export const LOGIN_PLAYER = 'LOGIN_PLAYER';
 export const LOGOUT_PLAYER = 'LOGOUT_PLAYER';
 
 // React Action Types
-export const GET_REACT_STATUS = 'GET_REACT_STATUS';
+export const GET_REACT_MESSAGE = 'GET_REACT_MESSAGE';
