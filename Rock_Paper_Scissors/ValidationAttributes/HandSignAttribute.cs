82596debfc7655881c3568fa8f1022a18c9615f9
TSM Techø <trottimusbg@gmail.com>
2019-02-10 22:42:49 +0200

    [HandSignAttribute] Add validation attribute to check if its rock, paper or scissors.

---
+++ Rock_Paper_Scissors/ValidationAttributes/HandSignAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace Rock_Paper_Scissors.ValidationAttributes
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+
+    public class HandSignAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string[] handSigns = { "Rock", "Paper", "Scissors" };
+
+            string handSign = (string)value;
+
+            if (!handSigns.Contains(handSign))
+            {
+                return new ValidationResult("Are you trying to cheat?");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
