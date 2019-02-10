e39e90146fa571abb20f0096acc393ce05573d3a
TSM Techø <trottimusbg@gmail.com>
2019-02-11 00:26:20 +0200

    [reactReducer] Change GET_REACT_STATUS case with GET_REACT_REPLICA which change state of the replica.

--- Rock_Paper_Scissors/ClientApp/src/store/reducers/reactReducer.js
+++ Rock_Paper_Scissors/ClientApp/src/store/reducers/reactReducer.js
@@ -1,9 +1,9 @@
-import { GET_REACT_STATUS } from '../actions/actionTypes';
+import { GET_REACT_REPLICA } from '../actions/actionTypes';
 
-export default function playerReducer(state = { message: '' }, action) {
+export default function playerReducer(state = { replica: 'React is ready to smash you.' }, action) {
     switch (action.type) {
-        case GET_REACT_STATUS:
-            return Object.assign({}, state);
+        case GET_REACT_REPLICA:
+            return Object.assign({}, state, { replica: action.replica });
         default:
             return state;
     }
