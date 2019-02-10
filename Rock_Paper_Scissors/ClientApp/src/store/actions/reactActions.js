0dc5f1a461e3f9555c479b4fca53a46e9e665b69
TSM Techø <trottimusbg@gmail.com>
2019-02-11 00:23:55 +0200

    [reactActions] Add get react replica action.

--- Rock_Paper_Scissors/ClientApp/src/store/actions/reactActions.js
+++ Rock_Paper_Scissors/ClientApp/src/store/actions/reactActions.js
@@ -1,12 +1,15 @@
 import react from '../../api/ReactAPI';
 import { errorAction } from './ajaxActions';
 import { logoutPlayerAction } from './playerActions';
+import { GET_REACT_REPLICA } from './actionTypes';
+import generateReactReplicas from '../../utils/generateReactReplicas';
 
 export function getReactStatusAction(payload) {
     return function(dispatch) {
         return react.getReactStatus(payload)
             .then(response => {
-
+                console.log(response);
+                dispatch(getReactReplicaAction(response.data.handSign, response.data.status))
             })
             .catch(error => {
                 const message = error.response.data.message;
@@ -18,4 +21,14 @@ export function getReactStatusAction(payload) {
                 }
             });
     }
+}
+
+
+export function getReactReplicaAction(handSign, status) {
+    const replica = generateReactReplicas(handSign, status);
+
+    return {
+        type: GET_REACT_REPLICA,
+        replica
+    };
 }
