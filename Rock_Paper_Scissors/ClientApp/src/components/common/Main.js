265daf97ef00eac46ba3786a6a8fc65dc706d9aa
TSM Techø <trottimusbg@gmail.com>
2019-02-11 21:37:38 +0200

    [Main] Add PlayersTable partial component.

--- Rock_Paper_Scissors/ClientApp/src/components/common/Main.js
+++ Rock_Paper_Scissors/ClientApp/src/components/common/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Fade from 'react-reveal/Fade';
+import PlayersTable from './partials/PlayersTable';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { getReactStatusAction } from '../../store/actions/reactActions';
@@ -22,38 +23,36 @@ class Main extends Component {
     }
 
     render() {
-        const loggedIn = this.props.loggedIn;
-
         return (
             <main>
-                {loggedIn && <div>
-                    <Fade left delay={800} duration={1000}>
-                        <p className="versus-text">VS</p>
-                    </Fade>
+                <Fade left delay={800} duration={1000}>
+                    <p className="versus-text">VS</p>
+                </Fade>
 
-                    <Fade left delay={1500}>
-                        <div className="rock-paper-scissors">
-                            <div className="rock" onClick={() => this.onClickHandler('Rock')}>
-                                <FontAwesomeIcon icon="hand-rock"/>
-                            </div>
-                            <div className="paper" onClick={() => this.onClickHandler('Paper')}>
-                                <FontAwesomeIcon icon="hand-paper"/>
-                            </div>
-                            <div className="scissors" onClick={() => this.onClickHandler('Scissors')}>
-                                <FontAwesomeIcon icon="hand-peace"/>
-                            </div>
+                <Fade left delay={1500}>
+                    <div className="rock-paper-scissors">
+                        <div className="rock" onClick={() => this.onClickHandler('Rock')}>
+                            <FontAwesomeIcon icon="hand-rock"/>
                         </div>
-                    </Fade>
-                </div>}
+                        <div className="paper" onClick={() => this.onClickHandler('Paper')}>
+                            <FontAwesomeIcon icon="hand-paper"/>
+                        </div>
+                        <div className="scissors" onClick={() => this.onClickHandler('Scissors')}>
+                            <FontAwesomeIcon icon="hand-peace"/>
+                        </div>
+                    </div>
+                </Fade>
+
+                <Fade bottom delay={1900}>
+                    <PlayersTable />
+                </Fade>
             </main>
         );
     }
 }
 
 function mapStateToProps(state) {
-    return {
-        loggedIn: state.player.loggedIn
-    };
+    return { }
 }
 
 function mapDispatchToProps(dispatch) {
