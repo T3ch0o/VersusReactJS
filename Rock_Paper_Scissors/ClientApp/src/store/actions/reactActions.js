324d8a4da4aed21c9254fe21b389e4cc8fd26e84
TSM Techø <trottimusbg@gmail.com>
2019-02-11 21:41:44 +0200

    [reactActions] Dispatch requestPlayersStatusAction after player win/loss/draw a game.

--- Rock_Paper_Scissors/ClientApp/src/store/actions/reactActions.js
+++ Rock_Paper_Scissors/ClientApp/src/store/actions/reactActions.js
@@ -1,15 +1,24 @@
 import react from '../../api/ReactAPI';
 import { errorAction } from './ajaxActions';
-import { logoutPlayerAction } from './playerActions';
+import { logoutPlayerAction, requestPlayersStatusAction } from './playerActions';
 import { GET_REACT_REPLICA } from './actionTypes';
 import generateReactReplicas from '../../utils/generateReactReplicas';
+
+export function getReactReplicaAction(handSign, status) {
+    const replica = generateReactReplicas(handSign, status);
+
+    return {
+        type: GET_REACT_REPLICA,
+        replica
+    };
+}
 
 export function getReactStatusAction(payload) {
     return function(dispatch) {
         return react.getReactStatus(payload)
             .then(response => {
-                console.log(response);
-                dispatch(getReactReplicaAction(response.data.handSign, response.data.status))
+                dispatch(getReactReplicaAction(response.data.handSign, response.data.status));
+                dispatch(requestPlayersStatusAction());
             })
             .catch(error => {
                 const message = error.response.data.message;
@@ -21,14 +30,4 @@ export function getReactStatusAction(payload) {
                 }
             });
     }
-}
-
-
-export function getReactReplicaAction(handSign, status) {
-    const replica = generateReactReplicas(handSign, status);
-
-    return {
-        type: GET_REACT_REPLICA,
-        replica
-    };
 }
