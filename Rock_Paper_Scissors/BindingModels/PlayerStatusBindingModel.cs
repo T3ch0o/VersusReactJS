64f976d6095c6635706cdb50ec579a74b88d0de4
TSM Techø <trottimusbg@gmail.com>
2019-02-11 21:35:26 +0200

    [PlayerStatusBindingModel] Add model to return all players status.

---
+++ Rock_Paper_Scissors/BindingModels/PlayerStatusBindingModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Rock_Paper_Scissors.BindingModels
+{
+    public class PlayerStatusBindingModel
+    {
+        public string Username { get; set; }
+
+        public int Wins { get; set; }
+
+        public int Losses { get; set; }
+
+        public string WinRate { get; set; }
+    }
+}
