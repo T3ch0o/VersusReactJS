3be16fb7a4dbe205aedb1913f08776093a880a71
TSM Techø <trottimusbg@gmail.com>
2019-02-11 21:38:01 +0200

    [actionTypes] Add get players action type.

--- Rock_Paper_Scissors/ClientApp/src/store/actions/actionTypes.js
+++ Rock_Paper_Scissors/ClientApp/src/store/actions/actionTypes.js
@@ -6,6 +6,7 @@ export const AJAX_SUCCESS = 'AJAX_SUCCESS';
 // Player Action Types
 export const LOGIN_PLAYER = 'LOGIN_PLAYER';
 export const LOGOUT_PLAYER = 'LOGOUT_PLAYER';
+export const GET_PLAYERS_STATUS = 'GET_PLAYERS_STATUS';
 
 // React Action Types
 export const GET_REACT_REPLICA = 'GET_REACT_REPLICA';
